708de331888ba507958544e969290b24691e2dfd
RioDev <mhafizhdsy@gmail.com>
2025-12-15 16:08:24 +0700

    Delete kuronime-api-main directory

--- kuronime-api-main/src/utils/animeByGenre.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import scrapeAnimeByGenre from '../lib/scrapeAnimeByGenre.js';
-const BASEURL = process.env.BASEURL   || 'https://otakudesu.best';
-const animeByGenre = async (genre, page = 1) => {
-    const response = await axios.get(`${BASEURL}/genres/${genre}/page/${page}`);
-    const result = scrapeAnimeByGenre(response.data);
-    return result;
-};
-export default animeByGenre;
